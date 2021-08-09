57e7ac2a88325bc9fa46ae178fb082ebd04f9826
yassine aourhebal <aourhebal.yassine@gmail.com>
2021-08-09 18:51:41 +0100

    setup socket.io

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import express from "express";
+import {Server} from "socket.io";
 
 //app setup
 const app = express();
 
 
-app.use(express.static('public'));
 const port = 5000;
 const server = app.listen(port, ()=>{
     console.log("app listen on port " + port);
 })
+
+
+//static files
+app.use(express.static('public'));
+
+//socket setup
+const io = new Server(server);
+
+io.on('connection',(socket)=>{
+    console.log('socket connection');
+});
